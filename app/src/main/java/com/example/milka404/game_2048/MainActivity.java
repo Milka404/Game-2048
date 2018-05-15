25264748aec296e93064fbf63ef52dfd4ce591fe
Favarish <Favarish@yandex.ru>
2018-05-16 01:44:49 +0700

    Add id in 'xml', create field initialization

--- app/src/main/java/com/example/milka404/game_2048/MainActivity.java
+++ app/src/main/java/com/example/milka404/game_2048/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.milka404.game_2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+
+public class  MainActivity extends AppCompatActivity {
+
+    Button[][] ButtonArray = new Button[4][4];
+
+    private void ArrayInit(Button[][] ButtonArray) {
+
+        ButtonArray[0][0] = (Button) findViewById(R.id.Button00);
+        ButtonArray[0][1] = (Button) findViewById(R.id.Button01);
+        ButtonArray[0][2] = (Button) findViewById(R.id.Button02);
+        ButtonArray[0][3] = (Button) findViewById(R.id.Button03);
+        ButtonArray[1][0] = (Button) findViewById(R.id.Button10);
+        ButtonArray[1][1] = (Button) findViewById(R.id.Button11);
+        ButtonArray[1][2] = (Button) findViewById(R.id.Button12);
+        ButtonArray[1][3] = (Button) findViewById(R.id.Button13);
+        ButtonArray[2][0] = (Button) findViewById(R.id.Button20);
+        ButtonArray[2][1] = (Button) findViewById(R.id.Button21);
+        ButtonArray[2][2] = (Button) findViewById(R.id.Button22);
+        ButtonArray[2][3] = (Button) findViewById(R.id.Button23);
+        ButtonArray[3][0] = (Button) findViewById(R.id.Button30);
+        ButtonArray[3][1] = (Button) findViewById(R.id.Button31);
+        ButtonArray[3][2] = (Button) findViewById(R.id.Button32);
+        ButtonArray[3][3] = (Button) findViewById(R.id.Button33);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayInit(ButtonArray);
     }
 }
+
